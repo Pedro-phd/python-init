7f01339960d0de7d4e0247c8bf0393cdb3b50766
pedro-phd <pedro_dominici@outlook.ccom>
2022-12-22 01:02:27 -0300

    começando com python!

---
+++ main.py
@@ -0,0 +1,14 @@
+#isso aqui é um comentario
+numero = 10;
+texto = "texto";
+booleanoValue = False;
+floatValue = 10.50;
+arrayValue = ['isso','é','um', 'array', 'alterar_esse_valor']
+arrayValue[4] = 'valor alterado!!'
+copiarArray = arrayValue[ : ]
+arrayValue.append('adicionar item') #igual o push de javascript
+arrayValue.pop('isso') # apaga o termo entre (), caso o mesmo esteja vazio, ele apaga o ultimo valor
+len(arrayValue) # retorna o tamanho do array
+def printOldAge(name,age):
+    print('Yor name is ' , name , 'and yor age is ' , age)
+printOldAge('pedro',21)
